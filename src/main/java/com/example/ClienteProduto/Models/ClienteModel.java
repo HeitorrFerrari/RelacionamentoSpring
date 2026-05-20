ed183a9dced8ae35829f57a0f58968ff6a32d330
HeitorrFerrari <heitorferrari9@gmail.com>
2026-05-20 20:18:57 -0300

    fetch: correcao

--- src/main/java/com/example/ClienteProduto/Models/ClienteModel.java
+++ src/main/java/com/example/ClienteProduto/Models/ClienteModel.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "TBL_CLIENTE")
-
 public class ClienteModel {
 
     @Id
@@ -16,7 +15,7 @@ public class ClienteModel {
     private Long id;
     private String nomeCliente;
 
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<ProdutoModel> produtoModels;
 
 }
